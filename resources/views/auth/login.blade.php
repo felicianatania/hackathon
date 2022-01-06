e816d150e6a70bc2140feb4475272657e4cd5822
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-01-06 17:54:30 +0700

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,6 +60,7 @@
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
+                                        {{-- . --}}
                                     </a>
                                 @endif
                             </div>
