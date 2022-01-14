0ddf2fbad836d2407652986a6cfe2364b8adba13
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-01-14 15:13:10 +0700

    update payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PaymentRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Payment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
     public function getGroupByIdPayment(Request $request){
         $groupId = $request->user()->id;
         $payment = DB::table('users')
-            ->join('payments')
-            ->select('')
-        return view('payment', ['groupId' => $groupId]);
+            // ->join('payments')
+            // ->select('users.id')
+            // ->where();
+            ->join('payments', 'git', '=', 'shops.shop_id')
+            ->where('customer_contact', $contact_no)
+            ->get();
+        return view('payment', ['groupId' => $groupId, 'payments'=> $payment]);
     }
 
     public function createPayment(PaymentRequest $request, $id){
