6bbfaecda4e1cfecf5930fe305aaa428a9eefa24
rifianfernando19 <rifian.fernando@binus.ac.id>
2022-01-14 13:28:01 +0700

    javascript

--- resources/views/adminPanel/updateMember.blade.php
+++ resources/views/adminPanel/updateMember.blade.php
@@ -244,5 +244,37 @@
       crossorigin="anonymous"
     ></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script>
+        let section = document.querySelectorAll("section");
+        let navLinks = document.querySelectorAll("nav a");
+
+        window.onscroll = () => {
+          //changing navbar
+          if (window.pageYOffset > 5) {
+            document.getElementById("navigationBar").style.backgroundColor =
+              "rgba(51, 66, 87, 0.4)";
+          } else {
+            document.getElementById("navigationBar").style.backgroundColor =
+              "rgba(51, 66, 87, 0)";
+          }
+
+          //scrollspy
+          section.forEach((sec) => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute("id");
+
+            if (top >= offset && top < offset + height) {
+              navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                  .querySelector("nav a[href*=" + id + "]")
+                  .classList.add("active");
+              });
+            }
+          });
+        };
+      </script>
   </body>
 </html>
